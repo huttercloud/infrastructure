24292b8a43ca56996107608672c7267bed2e6bac
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-11 22:08:15 +0100

    Add cname records in pihole

--- terraform/home/pi-hole/dns-records.tf
+++ terraform/home/pi-hole/dns-records.tf
@@ -1,18 +1,18 @@
+locals {
+    cname_records_node_a = [
+        "hello-world.hutter.cloud",
+        "wireguard.hutter.cloud",
+    ]
+}
+
 resource "pihole_dns_record" "pihole" {
   domain = "pihole.hutter.cloud"
   ip     = "192.168.30.253"
 }
-
-resource "pihole_dns_record" "hello-world" {
-  domain = "hello-world.hutter.cloud"
-  ip     = "192.168.30.61"
-}
-
 resource "pihole_dns_record" "nas" {
   domain = "nas.hutter.cloud"
   ip     = "192.168.30.17"
 }
-
 resource "pihole_dns_record" "homeassistant" {
   domain = "homeassistant.hutter.cloud"
   ip     = "192.168.30.182"
@@ -26,4 +26,12 @@ resource "pihole_dns_record" "node_a" {
 resource "pihole_dns_record" "node_b" {
   domain = "node-b.hutter.cloud"
   ip     = "192.168.30.34"
+}
+
+# cname records pointing to node a
+
+resource "pihole_cname_record" "node_a" {
+  for_each = toset(local.cname_records_node_a)
+  domain = each.value
+  target = "node-a.hutter.cloud"
 }
