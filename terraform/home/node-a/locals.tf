b093f1c3a6d1a1e5c2f6bbb8d329b3def435672c
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-11 22:07:56 +0100

    Add wireguard daemonset, attention, routing broken!

--- terraform/home/node-a/locals.tf
+++ terraform/home/node-a/locals.tf
@@ -1,6 +1,7 @@
 locals {
     external_dns_version = "v0.13.1"
     pi_hole_version = "2022.11.2"
+    wireguard_version = "1.0.20210914"
     pi_hole_external_ip = "192.168.30.253"
     cert_manager_email = "huttersebastian@gmail.com"
     cert_manager_zone_id = "Z3CBRYU3UXXFKT"
