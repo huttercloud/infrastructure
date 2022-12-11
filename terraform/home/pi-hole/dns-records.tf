0001770bc6ca4fb7d5465c74d8324ed6a704dee0
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-11 21:18:07 +0100

    Add additional dns records

--- terraform/home/pi-hole/dns-records.tf
+++ terraform/home/pi-hole/dns-records.tf
@@ -2,3 +2,28 @@ resource "pihole_dns_record" "pihole" {
   domain = "pihole.hutter.cloud"
   ip     = "192.168.30.253"
 }
+
+resource "pihole_dns_record" "hello-world" {
+  domain = "hello-world.hutter.cloud"
+  ip     = "192.168.30.61"
+}
+
+resource "pihole_dns_record" "nas" {
+  domain = "nas.hutter.cloud"
+  ip     = "192.168.30.17"
+}
+
+resource "pihole_dns_record" "homeassistant" {
+  domain = "homeassistant.hutter.cloud"
+  ip     = "192.168.30.182"
+}
+
+resource "pihole_dns_record" "node_a" {
+  domain = "node-a.hutter.cloud"
+  ip     = "192.168.30.61"
+}
+
+resource "pihole_dns_record" "node_b" {
+  domain = "node-b.hutter.cloud"
+  ip     = "192.168.30.34"
+}
