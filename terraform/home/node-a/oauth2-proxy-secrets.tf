75fc697e211046382964039947613090f7a82ffe
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-12 12:10:45 +0100

    Move credentials to locals

--- terraform/home/node-a/oauth2-proxy-secrets.tf
+++ terraform/home/node-a/oauth2-proxy-secrets.tf
@@ -2,12 +2,9 @@ resource "kubernetes_secret" "oauth2_proxy_secrets" {
   metadata {
     name = "oauth2-proxy-secrets"
   }
-
-
   data = {
-    client_id     = data.terraform_remote_state.auth0.outputs.oauth2_proxy_client_id
-    client_secret = data.terraform_remote_state.auth0.outputs.oauth2_proxy_client_secret
-    # sharing the same cookie secret is a no go ... but its a home network so ... aeh
-    cookie_secret = var.oauth2_proxy_cookie_secret
+    client_id     = local.oauth2_proxy_client_id
+    client_secret = local.oauth2_proxy_client_secret
+    cookie_secret = local.oauth2_proxy_cookie_secret
   }
 }
