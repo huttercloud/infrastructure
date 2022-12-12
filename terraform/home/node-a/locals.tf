d55d87ab02ca0becd6dd9fabf5dde805d66b5b91
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-12 12:10:35 +0100

    Move credentials to locals

--- terraform/home/node-a/locals.tf
+++ terraform/home/node-a/locals.tf
@@ -1,8 +1,13 @@
 locals {
-    # aws credentials
+    # aws credentials for dns
     access_key_id_dns = data.terraform_remote_state.aws-root-global.outputs.user_dns_access_key_id
     secret_access_key_dns = data.terraform_remote_state.aws-root-global.outputs.user_dns_secret_access_key
     
+    # auth0 credentials for ouath2-proxy
+    oauth2_proxy_client_id = data.terraform_remote_state.auth0.outputs.oauth2_proxy_client_id
+    oauth2_proxy_client_secret = data.terraform_remote_state.auth0.outputs.oauth2_proxy_client_secret
+    oauth2_proxy_cookie_secret = var.oauth2_proxy_cookie_secret
+
     # application versions
     external_dns_version = "v0.13.1"
     pi_hole_version = "2022.11.2"
