8f6ebd244e94fb27382f9942ffae5db233de740c
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-12 22:18:58 +0100

    Cleanup tf code to use external-secrets where possible

--- terraform/home/node-a/cluster-issuer.tf
+++ terraform/home/node-a/cluster-issuer.tf
@@ -1,15 +1,47 @@
-
-
-resource "kubernetes_secret" "cert_manager_cluster_issuer_dns" {
-  metadata {
-    name = "cluster-issuer-aws"
-    namespace = "cert-manager"
+resource "kubernetes_manifest" "cluster_issuer_aws" {
+  manifest = {
+    "apiVersion" = "external-secrets.io/v1beta1"
+    "kind" = "ExternalSecret"
+    "metadata" = {
+      "name" = "cluster-issuer-aws"
+      "namespace" = "cert-manager"
+      "labels" = {
+        "app.kubernetes.io/name" = "cluster-issuer"
+      }
+    }
+    "spec" = {
+      "data" = [
+        {
+          "remoteRef" = {
+            "key" = "hutter-cloud-dns-access-key-id"
+          }
+          "secretKey" = "accesskey"
+        },
+        {
+          "remoteRef" = {
+            "key" = "hutter-cloud-dns-secret-access-key"
+          }
+          "secretKey" = "secretkey"
+        },
+      ]
+      "refreshInterval" = "5m"
+      "secretStoreRef" = {
+        "kind" = "ClusterSecretStore"
+        "name" = "ssm"
+      }
+      "target" = {
+        "template" = {
+          "data" = {
+            "accessKey" = "{{ .accesskey }}"
+            "secretKey" = "{{ .secretkey }}"
+          }
+          "engineVersion" = "v2"
+        }
+      }
+    }
   }
+}
 
-  data = {
-    secret-access-key = local.secret_access_key_dns
-  }
-} 
 
 resource "kubernetes_manifest" "cert_manager_cluster_issuer_dns" {
   manifest = {
@@ -30,11 +62,15 @@ resource "kubernetes_manifest" "cert_manager_cluster_issuer_dns" {
                     cnameStrategy = "Follow"
                     route53 = {
                         region = "eu-central-1"
+                        # not supported in microk8s cert-manager v1.8
+                        # accessKeyIDSecretRef = {
+                        #     name = "cluster-issuer-aws"
+                        #     key = "accessKey"
+                        # }
                         accessKeyID = local.access_key_id_dns
-                        hostedZoneID = local.cert_manager_zone_id
                         secretAccessKeySecretRef = {
-                            name = kubernetes_secret.cert_manager_cluster_issuer_dns.metadata[0].name
-                            key = "secret-access-key"
+                            name = "cluster-issuer-aws"
+                            key = "secretKey"
                         }
                     }
                     
@@ -44,4 +80,3 @@ resource "kubernetes_manifest" "cert_manager_cluster_issuer_dns" {
     }
   }
 }
-
--- terraform/home/node-a/oauth2-proxy-secrets.tf
+++ terraform/home/node-a/oauth2-proxy-secrets.tf
@@ -1,10 +1,47 @@
-resource "kubernetes_secret" "oauth2_proxy_secrets" {
-  metadata {
-    name = "oauth2-proxy-secrets"
-  }
-  data = {
-    client_id     = local.oauth2_proxy_client_id
-    client_secret = local.oauth2_proxy_client_secret
-    cookie_secret = local.oauth2_proxy_cookie_secret
+resource "kubernetes_manifest" "oauth2_proxy_secrets" {
+  manifest = {
+    "apiVersion" = "external-secrets.io/v1beta1"
+    "kind" = "ExternalSecret"
+    "metadata" = {
+      "name" = "oauth2-proxy-secrets"
+      "namespace" = "default"
+    }
+    "spec" = {
+      "data" = [
+        {
+          "remoteRef" = {
+            "key" = "hutter-cloud-service-oauth2-proxy-client-id"
+          }
+          "secretKey" = "clientid"
+        },
+        {
+          "remoteRef" = {
+            "key" = "hutter-cloud-service-oauth2-proxy-client-secret"
+          }
+          "secretKey" = "clientsecret"
+        },
+        {
+          "remoteRef" = {
+            "key" = "hutter-cloud-service-oauth2-proxy-cookie-secret"
+          }
+          "secretKey" = "cookiesecret"
+        },
+      ]
+      "refreshInterval" = "5m"
+      "secretStoreRef" = {
+        "kind" = "ClusterSecretStore"
+        "name" = "ssm"
+      }
+      "target" = {
+        "template" = {
+          "data" = {
+            "client_id" = "{{ .clientid }}"
+            "client_secret" = "{{ .clientsecret }}"
+            "cookie_secret" = "{{ .cookiesecret }}"
+          }
+          "engineVersion" = "v2"
+        }
+      }
+    }
   }
 }
