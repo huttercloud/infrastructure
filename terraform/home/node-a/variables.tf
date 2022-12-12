49779afbed986582fa2bbcc70ee96e692a7a08d4
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-12 11:59:37 +0100

    Create iam users in aws and inherit credentials

--- terraform/home/node-a/variables.tf
+++ terraform/home/node-a/variables.tf
@@ -1,13 +1,3 @@
-variable "external_dns_aws_access_key_id" {
-    sensitive = true
-    type = string
-}
-
-variable "external_dns_aws_secret_access_key" {
-    sensitive = true
-    type = string
-}
-
 variable "pi_hole_webpassword" {
     sensitive = true
     type = string
--- terraform/home/node-b/variables.tf
+++ terraform/home/node-b/variables.tf
@@ -1,9 +1,4 @@
-variable "external_dns_aws_access_key_id" {
-    sensitive = true
-    type = string
-}
-
-variable "external_dns_aws_secret_access_key" {
+variable "pi_hole_webpassword" {
     sensitive = true
     type = string
 }
