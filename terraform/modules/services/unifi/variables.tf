d194b52598a9c7171b4a6d23d28d596310f387b3
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-18 10:18:26 +0100

    Add unifi service

---
+++ terraform/modules/services/unifi/variables.tf
@@ -0,0 +1,16 @@
+variable "storage_class_name" {
+  type = string
+}
+
+variable "unifi_version" {
+  type = string
+}
+
+variable "unifi_external_ip" {
+  type = string
+  
+}
+
+variable "unifi_hostname" {
+  type = string
+}
