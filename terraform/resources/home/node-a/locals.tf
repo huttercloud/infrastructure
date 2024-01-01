c02c64e5e635e39b2b32f52eba364e8dcb07f5ba
sebastianhutter <sebastianhutter@users.noreply.github.com>
2024-01-01 03:17:25 +0000

    chore(update): Update versions

--- terraform/resources/home/node-a/locals.tf
+++ terraform/resources/home/node-a/locals.tf
@@ -1,7 +1,7 @@
 locals {
   # application versions
   external_dns_version           = "v0.13.4"
-  external_secrets_version       = "0.9.10"
+  external_secrets_version       = "0.9.11"
   pi_hole_version                = "2023.11.0"
   wireguard_version              = "1.0.20210914"
   unifi_version                  = "8.0.24"
--- terraform/resources/home/node-b/locals.tf
+++ terraform/resources/home/node-b/locals.tf
@@ -1,8 +1,8 @@
 locals {
   # application versions
   external_dns_version           = "v0.13.4"
-  external_secrets_version       = "0.9.10"
-  argo_cd_version                = "5.51.6"
+  external_secrets_version       = "0.9.11"
+  argo_cd_version                = "5.52.0"
 
   # cert manager configuration
   cert_manager_email = "huttersebastian@gmail.com"
