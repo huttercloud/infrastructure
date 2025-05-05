d07c3db9b0199fc9cb27e52fe6146b312ea099cb
sebastianhutter <sebastianhutter@users.noreply.github.com>
2025-05-05 03:43:01 +0000

    chore(update): Update versions

--- terraform/resources/home/node-b/locals.tf
+++ terraform/resources/home/node-b/locals.tf
@@ -2,7 +2,7 @@ locals {
   # application versions
   external_dns_version           = "v0.13.5"
   external_secrets_version       = "0.16.1"
-  argo_cd_version                = "7.8.28"
+  argo_cd_version                = "7.9.0"
 
   # cert manager configuration
   cert_manager_email = "huttersebastian@gmail.com"
